84446817e466b682a086694ad1fc1e3a860171e2
Abhivamsh <kalwalaabhivamsh@gmail.com>
2024-09-26 02:29:32 +0530

    Fix Home.jsx

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
-import Cards from '../CardF'; // Ensure you import the Cards component
+
 
 function Home() { // Change 'home' to 'Home'
     return (
         <>
             <HeroSection />
-            <Cards /> {/* Change 'cards' to 'Cards' */}
+           
         </>
     );
 }
