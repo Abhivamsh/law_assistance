4c027f77739b57cf94ca1ddb7e86b2754a10054e
Abhivamsh <kalwalaabhivamsh@gmail.com>
2024-09-26 03:12:32 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend', // Ensure this points to your frontend directory
+  root: 'frontend',
+  port:'3000', // Ensure this points to your frontend directory
   build: {
     outDir: 'build', // This is where the build will be output
     emptyOutDir: true // Clear the output directory before building
